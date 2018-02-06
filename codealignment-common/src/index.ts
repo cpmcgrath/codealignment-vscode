47bdd1a489c3925c43d3a0b09ed8b22508d97e72
Chris McGrath <admin@cpmcgrath.com>
2018-02-06 11:36:29 +1000

    #21 Export properly

--- codealignment-common/src/index.ts
+++ codealignment-common/src/index.ts
@@ -2,3 +2,11 @@ import { Alignment            } from './business/Alignment'
 import { GeneralScopeSelector } from './business/selectors/GeneralScopeSelector';
 import { RegexDelimiterFinder } from './business/delimiterFinders/RegexDelimiterFinder';
 import { Options              } from './business/Options';
+
+export
+{
+    Alignment,
+    GeneralScopeSelector,
+    RegexDelimiterFinder,
+    Options
+};
